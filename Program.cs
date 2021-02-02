fe29c4db701f263634535b6fd8d0596370530fe6
Rishant Dutt <rishant.dutt96@gmail.com>
2021-02-02 16:11:41 -0500

    Reading database correctly

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Microsoft.Data.Sqlite;
+using static System.Console;
 
 namespace Code
 {
@@ -6,7 +8,25 @@ namespace Code
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var connStr =  @".\SurfaceRoughnessDB.db3";
+            var connectionStringBuilder = new SqliteConnectionStringBuilder() {
+                DataSource=connStr,
+                Mode=SqliteOpenMode.ReadOnly
+            };
+            using (var connection = new SqliteConnection(connectionStringBuilder.ConnectionString))
+            {
+                connection.Open();
+
+                string measurements = "SELECT * FROM Measurements LIMIT 10";
+
+                using var cmd = new SqliteCommand(measurements, connection);
+                SqliteDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    WriteLine($"{reader.GetInt32(0)} {reader.GetInt32(1)} {reader.GetFloat(2)} {reader.GetFloat(3)} {reader.GetFloat(4)}");
+                }
+            }
         }
     }
 }
