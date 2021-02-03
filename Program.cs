3f2e661f3afcd67e25360f26ed24d806aad8c5fa
Rishant Dutt <rishant.dutt96@gmail.com>
2021-02-02 19:16:30 -0500

    Successfully generating reports

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿ using System;
+using System.Text;
 using System.Collections.Generic;
 using Microsoft.Data.Sqlite;
+using System.IO;
 using static System.Console;
 
 namespace Code
@@ -8,9 +10,14 @@ namespace Code
 
     class Program
     {
+
         static void Main(string[] args)
         {
-            var connStr =  @".\SurfaceRoughnessDB.db3";
+            Write("Enter the database location (press Enter, leave empty for '.\\SurfaceRoughnessDB.db3'): ");
+            string dbPath = ReadLine();
+            
+
+            var connStr =  dbPath == "" ? @".\SurfaceRoughnessDB.db3" : $@"{dbPath}";
             var connectionStringBuilder = new SqliteConnectionStringBuilder() {
                 DataSource=connStr,
                 Mode=SqliteOpenMode.ReadOnly
@@ -18,7 +25,7 @@ namespace Code
             using (var connection = new SqliteConnection(connectionStringBuilder.ConnectionString))
             {
                 connection.Open();
-                string measurements = "SELECT test_uid, x, y, height FROM Measurements LIMIT 1005"; 
+                string measurements = "SELECT test_uid, x, y, height FROM Measurements"; 
 
                 using var getMeasurementsCmd = new SqliteCommand(measurements, connection);
                 SqliteDataReader reader = getMeasurementsCmd.ExecuteReader();
@@ -80,7 +87,7 @@ namespace Code
                     }
                 }
 
-                string measurementsHeightsOnly = "SELECT test_uid, height FROM Measurements LIMIT 1005"; 
+                string measurementsHeightsOnly = "SELECT test_uid, height FROM Measurements"; 
 
                 using var getHeightsOnlyCmd = new SqliteCommand(measurementsHeightsOnly, connection);
                 reader = getHeightsOnlyCmd.ExecuteReader();
@@ -105,6 +112,51 @@ namespace Code
                         throw new Exception("Something went wrong");
                     }
                 }
+
+                string getTests = "SELECT * FROM Tests"; 
+
+                using var getTestCmd = new SqliteCommand(getTests, connection);
+                reader = getTestCmd.ExecuteReader();
+
+                var csv = new StringBuilder();
+                csv.AppendLine($"test_uid, sTime, PlaneID, Operator, min_height, min_height_X, min_height_Y, max_height, max_height_X, max_height_Y, mean_height, range_height, avg_roughness, rms_roughness");
+
+                while (reader.Read()) {
+                    try {
+                    int test_uid = reader.GetInt32(0);
+                        if (testInfo[test_uid].count < 1000) continue;
+                        
+                        DateTime test_sTime = reader.GetDateTime(1);
+                        string test_planeId = reader.GetString(2);
+                        string test_operator = "<UNKNOWN>";
+                        try {
+                        test_operator = reader.GetString(3);
+                        }
+                        catch (InvalidOperationException) {}
+
+                        var min_height = testInfo[test_uid].minHeight;
+                        var min_height_X = testInfo[test_uid].minPos.minHeightX;
+                        var min_height_Y = testInfo[test_uid].minPos.minHeightY;
+                        
+                        var max_height = testInfo[test_uid].maxHeight;
+                        var max_height_X = testInfo[test_uid].maxPos.maxHeightX;
+                        var max_height_Y = testInfo[test_uid].maxPos.maxHeightY;
+
+                        var mean_height = testInfo[test_uid].heightSum / testInfo[test_uid].count;
+                        var range_height = testInfo[test_uid].maxHeight - testInfo[test_uid].minHeight;
+
+                        var avg_roughness = errors[test_uid].absErr / testInfo[test_uid].count;
+                        var rms_roughness = Math.Pow(
+                            (errors[test_uid].rmsErr / testInfo[test_uid].count), 0.5);
+
+                        var newLine = $"{test_uid},{test_sTime},{test_planeId},{test_operator},{min_height},{min_height_X},{min_height_Y},{max_height},{max_height_X},{max_height_Y},{mean_height},{range_height},{avg_roughness},{rms_roughness}";
+                        csv.AppendLine(newLine);
+                    }
+                    catch (KeyNotFoundException) {}
+                }
+
+                File.WriteAllText(".\\roughness_report.csv", csv.ToString());
+                WriteLine($"Report generated at {connStr}");
             }
         }
     }
